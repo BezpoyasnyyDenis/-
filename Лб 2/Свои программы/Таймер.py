0459298d59946977a7d2d430d788014b2b5881e1
BezpoyasnyyDenis <D.Bezpoyasnyy_FIT_8_21_B_d@knute.edu.ua>
2021-09-20 00:41:13 +0300

    .

---
+++ Лб 2/Свои программы/Таймер.py
@@ -0,0 +1,20 @@
+import time
+
+while True:
+    i = 0
+    ii = 0
+    iii = 0
+    time_user = int(input("Введіть кількість секунд: "))
+    for q in range(time_user):
+        time.sleep(1)
+        i += 1
+        print("Пройшло секунд:", i)
+        if(i % 60) == 0:
+            ii += 1
+            print("Пройшло хвилин:", ii)
+        if(i % 3600) == 0:
+            iii += 1
+            print("Пройшло годин:", iii)
+
+    print("Час закінчився!")
+    
