7d3ec69e37bc9b02b0865625999315a323ae0316
BezpoyasnyyDenis <D.Bezpoyasnyy_FIT_8_21_B_d@knute.edu.ua>
2021-09-19 19:31:55 +0300

    .

---
+++ Лб 2/hello world.py
@@ -0,0 +1,11 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Hello world!")
+Hello world!
+>>> print (9*3)
+27
+>>> print (24**3)
+13824
+>>> print (5+17)
+22
+>>> 
