2ff5c88ed4aa2c41862fc2981849d8a0b33703d7
Bezpoyasny Denis <D.Bezpoyasnyy_FIT_8_21_B_d@knute.edu.ua>
2021-11-08 23:58:14 +0200

    .

--- Лб 6/Lab 6.py
+++ Лб 6/Lab 6.py
@@ -1,46 +1,45 @@
 class Student:
-    
-    def _init_(self,full_name = "", group_number = 0, progress = []):
-        self.full_name = full_name
+
+    def __init__(self, name = "", group_number = 0, progress = []):
+        self.name = name
         self.group_number = group_number
         self.progress = progress
     def __repr__(self):
-                return repr(("Student: " + self.full_name + " Group: " + self.group_number))
+                return repr(("Студент: " + self.name + "Група: " + self.group.number))
 
-    def set_full_name(self, full_name):
-         self.full_name = full_name
+    def set_name(self, name):
+        self.name = name
     def set_group_number(self, group_number):
-         self.group_numder = group_number
+        self.group_number = group_number
     def set_progress(self, progress):
         self.progress = progress
 
-    def get_full_name(self):
-         return self.full_name
+    def get_name(self):
+        return self.name
     def get_group_number(self):
-         return self.group_number
-
+        return self.group_number
     def get_progress(self):
-         return self.progress
+        return self.progress
 
 st_size = 10
 students = []
 for i in range(st_size):
     st = Student()
-    print("Впишіть ім'я: ")
-    st.full_name = input()
-    print("Впишіть номер групи: ")
+    print("Введіть ім'я та прізвище: ")
+    st.name = input()
+    print("Введіть номер групи: ")
     st.group_number = input()
     print("Введіть оцінку: ")
     st.progress = []
     for i in range(5):
-        score = int(input()) 
-        st.progress.append(score)
-    students.appens(st)
-print("Sorted students:")
+        score = int(input())
+        st.progress.append(st)
+print("Відсортовані студенти: ")
 for st in students:
     print(st)
 
-students = sorted(students, key=lambda student: student.full_name)
-print("Sorted students:")
+students = sorted(students , key = lambda student: student.name)
+print("Відсортовані студенти: ")
 for st in students:
     print(st)
+    
